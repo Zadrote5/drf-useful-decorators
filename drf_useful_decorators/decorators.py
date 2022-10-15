d3417e54fde6306812ad15aa61693128fc548d1d
user <a.tikhonovich@verweb.dev>
2022-10-15 16:28:36 +0300

    fixes

--- drf_useful_decorators/decorators.py
+++ drf_useful_decorators/decorators.py
@@ -3,12 +3,17 @@ from django.db import transaction
 from rest_framework import status
 from .methods import get_response
     
-    
-def exceptions_endpoint(endpoint):
-    @functools.wraps(endpoint)
-    def inner(request, *args, **kwargs):
-        try:
-            return transaction.atomic(endpoint(request, *args, **kwargs))
-        except Exception as e:
-                return get_response(str(e), status.HTTP_400_BAD_REQUEST)
-    return inner
+
+def exceptions_endpoint(atomic: bool=True):    
+    def wrapper(endpoint):
+        @functools.wraps(endpoint)
+        def inner(request, *args, **kwargs):
+            try:
+                if atomic:
+                    with transaction.atomic():
+                        return endpoint(request, *args, **kwargs)
+                return endpoint(request, *args, **kwargs)
+            except Exception as e:
+                    return get_response(str(e), status.HTTP_400_BAD_REQUEST)
+        return inner
+    return wrapper
